114ab2c99f7a546181ee27996db87149156cf3f7
Seokjun Kim <colus001@me.com>
2015-11-22 17:28:19 +0900

    Update material-icons and add special message
    - Package will be removed when original package is updated

--- client/chat/Reciever.jsx
+++ client/chat/Reciever.jsx
@@ -1,3 +1,18 @@
+const SPECIAL_MESSAGES = [
+  {
+    key: 'STOP_SMOKING',
+    message: '버스정류장에서는 담배를 피시면 안되요!\n당장 꺼주세요!',
+  },
+  {
+    key: 'PERVERT_ALERT',
+    message: '주위에 치한때문에 불편한 분이 있습니다.\n도움이 사람이 있는지 찾아보세요.',
+  },
+  {
+    key: 'NEED_YOUR_HELP',
+    message: '긴급하게 도움을 필요로 하시는 분이 있습니다.\n주위를 둘러봐주세요.',
+  }
+]
+
 Receiver = React.createClass({
   mixins: [ReactMeteorData],
 
@@ -16,28 +31,58 @@ Receiver = React.createClass({
     Messages.remove(this.data.messages[0]._id);
   },
 
-  render() {
-    var message = (this.data.messages.length > 0)
-      ? this.data.messages[0].message
-      : '메시지가 없습니다';
-
+  renderNormalMessage(message) {
     return (
-      <div className="container" style={{marginTop: '20%', whiteSpace: 'pre'}}>
-        <div className="row">
-           <div className="col s12">
-             <div className="card">
-               <div className="card-content">
-                 <span className="card-title">메시지</span>
-                 <p>
-                   {message}
-                 </p>
-               </div>
-               <div className="card-action">
-                 <a href="#" onClick={this.removeLatest}>삭제</a>
-               </div>
+      <div className="row">
+         <div className="col s12">
+           <div className="card blue-grey darken-1">
+             <div className="card-content white-text">
+               <span className="card-title">메시지</span>
+               <p>
+                 {message}
+               </p>
+             </div>
+             <div className="card-action">
+               <a href="#" onClick={this.removeLatest}>삭제</a>
              </div>
            </div>
          </div>
+       </div>
+    )
+  },
+
+  renderSpecialMessage(message) {
+    return (
+      <div className="row">
+         <div className="col s12">
+           <div className="card pink darken-1">
+             <div className="card-content white-text">
+               <span className="card-title">특별한 메시지</span>
+               <p>
+                 {message}
+               </p>
+             </div>
+             <div className="card-action">
+               <a href="#" onClick={this.removeLatest}>삭제</a>
+             </div>
+           </div>
+         </div>
+       </div>
+    )
+  },
+
+  render() {
+    var message = (this.data.messages.length > 0)
+      ? this.data.messages[0].message.trim()
+      : '메시지가 없습니다';
+    var specialMessage = _.find(SPECIAL_MESSAGES, {key: message});
+    var rendering = (specialMessage)
+      ? this.renderSpecialMessage(specialMessage.message)
+      : this.renderNormalMessage(message);
+
+    return (
+      <div className="container" style={{marginTop: '20%', whiteSpace: 'pre'}}>
+        {rendering}
       </div>
     )
   }})
--- client/chat/Sender.jsx
+++ client/chat/Sender.jsx
@@ -20,6 +20,20 @@ Sender = React.createClass({
     Materialize.toast('메시지를 전송하였습니다!', 3000);
   },
 
+  sendMessage(message, evt) {
+    var message = message.trim();
+    if (message.length === 0) return;
+
+    console.log(message);
+
+    Messages.insert({
+      createdAt: new Date(),
+      message: message,
+    });
+    this.refs.message.value = '';
+    Materialize.toast('특별한 메시지를 전송하였습니다!', 3000);
+  },
+
   render() {
     return (
       <div className="container">
@@ -35,6 +49,31 @@ Sender = React.createClass({
             <i className="material-icons right">send</i>
           </button>
         </form>
+
+        <div className="row">
+          <div className="col s12">
+            <h6>특별한 메시지를 보내주세요</h6>
+          </div>
+          <div className="col s12">
+            <div className="row">
+              <div className="col s4">
+                <button style={{width: '100%'}} className="btn-large orange waves-effect waves-light" onClick={this.sendMessage.bind(this, 'STOP_SMOKING')}>
+                  <i className="material-icons left">smoke_free</i> 담배 좀 꺼주세요
+                </button>
+              </div>
+              <div className="col s4">
+                <button style={{width: '100%'}} className="btn-large red waves-effect waves-light" onClick={this.sendMessage.bind(this, 'PERVERT_ALERT')}>
+                  <i className="material-icons left">report</i> 치한이 있어요
+                </button>
+              </div>
+              <div className="col s4">
+                <button style={{width: '100%'}} className="btn-large indigo waves-effect waves-light" onClick={this.sendMessage.bind(this, 'NEED_YOUR_HELP')}>
+                  <i className="material-icons left">live_help</i> 도움이 필요해요
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
